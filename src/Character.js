e24255000033804264e746a422070468665dca24
Andrea Ruggiero <andrea@pupax.me>
2019-01-25 21:24:07 +0100

    Improve moving

--- src/Character.js
+++ src/Character.js
@@ -1,16 +1,22 @@
 import './Character.css'
+import { clamp } from './Utils'
 
 class Character {
 
     element = null
     x = 0
     y = 0
-    speed = 10
+    speed = 100
 
     constructor() {
         document.writeln('<div id="game-character"></div>')
         this.element = document.getElementById('game-character')
         document.onkeydown = (e) => this.keyListener(e)
+        window.onresize = () => {
+            this.fixPg()
+            this.reDraw()
+        }
+        this.reDraw()
     }
     
     reDraw() {
@@ -30,7 +36,15 @@ class Character {
         else if (e.keyCode == '39') {
            this.x += this.speed
         }
+
+        this.fixPg();
+
         this.reDraw()
+    }
+
+    fixPg() {
+        this.x = clamp(this.x, 0, window.innerWidth - this.element.offsetWidth)
+        this.y = clamp(this.y, 0, window.innerHeight - this.element.offsetHeight)
     }
 }
 
---
+++ src/Utils.js
@@ -0,0 +1,10 @@
+
+const clamp = (v, min, max) => {
+    if (v < min) return min;
+    if (v > max) return max;
+    return v;
+}
+
+export {
+    clamp
+}
