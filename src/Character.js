d880be97028076f3026e86c654b601c45cf32a68
Andrea Ruggiero <andrea@pupax.me>
2019-01-25 21:16:12 +0100

    Add  moving character

--- src/Character.js
+++ src/Character.js
@@ -3,10 +3,34 @@ import './Character.css'
 class Character {
 
     element = null
+    x = 0
+    y = 0
+    speed = 10
 
     constructor() {
         document.writeln('<div id="game-character"></div>')
         this.element = document.getElementById('game-character')
+        document.onkeydown = (e) => this.keyListener(e)
+    }
+    
+    reDraw() {
+        this.element.style = `top: ${this.y}px; left: ${this.x}px`
+    }
+
+    keyListener(e) {
+        if (e.keyCode == '38') {
+            this.y -= this.speed
+        }
+        else if (e.keyCode == '40') {
+            this.y += this.speed
+        }
+        else if (e.keyCode == '37') {
+           this.x -= this.speed
+        }
+        else if (e.keyCode == '39') {
+           this.x += this.speed
+        }
+        this.reDraw()
     }
 }
 
