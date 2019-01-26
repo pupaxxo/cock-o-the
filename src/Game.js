00c9758e9031130eafb7b6badaa2cafd33e5e2c3
david98 <davidevolta98@gmail.com>
2019-01-26 04:54:05 +0100

    start developing timer

--- src/Game.js
+++ src/Game.js
@@ -1,12 +1,15 @@
 import Character from './Character'
 import './Game.css'
 import HomeFinder from './HomeFinder'
+import UIManager from './UIManager.js'
+import Usable from './Usable.js'
 
 class Game {
 
     started = false
 
     character = null
+    UIManager = null
     homeFinder = null
 
     delayBeforeStart = 2500
@@ -29,8 +32,9 @@ class Game {
     start() {
         this.started = true
         setTimeout(() => {this.character = new Character()}, this.delayBeforeStart)
-
+        this.UIManager = new UIManager()
         this.homeFinder = new HomeFinder()
+        this.UIManager.startTimer(10)
     }
 
     stop() {
